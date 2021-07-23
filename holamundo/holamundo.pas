8795b9c78b8c33e505b94f7f6bdc6e0f7cda3c24
Mauro Parra-Miranda <mauropm@me.com>
2021-07-22 19:58:21 -0500

    ¡Hola Mundo!
    El típico programa de hola mundo, que nos ayuda a probar que el ambiente de desarrollo este bien configurado.
    
    Les invito a mirar la imagen, es impresionante lo poco que ocupa el compilador, en términos modernos de 16GB de RAM en mi thinkpad, el compilador solo ocupo 5.5mb de RAM.

---
+++ holamundo/holamundo.pas
@@ -0,0 +1,13 @@
+(*
+ * hola mundo
+ * El programa hola mundo es un tipico programa de prueba, que te permite ver si tu
+ * ambiente de desarrollo esta bien configurado.
+ * Es la unidad minima de prueba.
+ *
+ *)
+
+program holamundo;
+begin
+ writeln('Hola Mundo!');
+end.
+
